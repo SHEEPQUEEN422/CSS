70694127842621e282bc1777ec1c5a206e42f955
Helen Junyi <SHEEPQUEEN422@gmail.com>
2024-10-12 17:10:11 -0700

    Update and rename final_project.py to Final project.py

--- Final project.py
+++ Final project.py
@@ -1,14 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 get_ipython().system('pip install wordcloud')
-
-
-# In[4]:
-
 
 import pandas as pd
 
@@ -34,58 +24,26 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix , classification_report ,accuracy_score,ConfusionMatrixDisplay
 
-
-# In[5]:
-
-
 #Validation dataset
 val_data=pd.read_csv("../a_Week04_Lecture04_a/twitter_validation.csv", header=None)
 #Full dataset for Train-Test
 train_data=pd.read_csv("../a_Week04_Lecture04_a/twitter_training.csv", header=None)
 
-
-# In[6]:
-
-
 train_data.columns=['id','information','type','text']
 train_data.head()
-
-
-# In[7]:
-
 
 val_data.columns=['id','information','type','text']
 val_data.head()
 
-
-# In[8]:
-
-
 train_data.isna().sum()
-
-
-# In[9]:
-
 
 val_data.isna().sum()
 
-
-# In[10]:
-
-
 train_data.dropna(inplace=True)
-
-
-# In[11]:
-
 
 count=train_data['type'].value_counts().reset_index()
 count.columns = ['type', 'count']
 count
-
-
-# In[12]:
-
 
 fig = px.bar(count, x='type', y='count', title='Type Counts',color='type', color_discrete_sequence=px.colors.sequential.Plasma)
 fig.update_layout(
@@ -96,10 +54,6 @@ fig.update_layout(
 )
 fig.show()
 
-
-# In[13]:
-
-
 #Text transformation
 train_data["lower"]=train_data.text.str.lower() #lowercase
 train_data["lower"]=[str(data) for data in train_data.lower] #converting all to string
@@ -108,15 +62,7 @@ val_data["lower"]=val_data.text.str.lower() #lowercase
 val_data["lower"]=[str(data) for data in val_data.lower] #converting all to string
 val_data["lower"]=val_data.lower.apply(lambda x: re.sub('[^A-Za-z0-9 ]+', ' ', x)) #regex
 
-
-# In[14]:
-
-
 train_data.tail()
-
-
-# In[15]:
-
 
 word_cloud_text = ''.join(train_data[train_data["type"]=="Positive"].lower)
 #Creation of wordcloud
@@ -134,10 +80,6 @@ plt.figure(figsize=(10,10))
 plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
 plt.show()
-
-
-# In[16]:
-
 
 word_cloud_text = ''.join(train_data[train_data["type"]=="Negative"].lower)
 #Creation of wordcloud
@@ -157,10 +99,6 @@ plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
 plt.show()
 
-
-# In[17]:
-
-
 word_cloud_text = ''.join(train_data[train_data["type"]=="Irrelevant"].lower)
 #Creation of wordcloud
 wordcloud = WordCloud(
@@ -177,10 +115,6 @@ plt.figure(figsize=(10,10))
 plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
 plt.show()
-
-
-# In[18]:
-
 
 word_cloud_text = ''.join(train_data[train_data["type"]=="Neutral"].lower)
 #Creation of wordcloud
@@ -200,16 +134,8 @@ plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
 plt.show()
 
-
-# In[20]:
-
-
 plot1=train_data.groupby(by=["information","type"]).count().reset_index()
 plot1[['information',  'id']]
-
-
-# In[21]:
-
 
 fig = px.bar(
     plot1,
@@ -234,19 +160,11 @@ fig.update_layout(
 # Show the figure
 fig.show()
 
-
-# In[22]:
-
-
 #Text splitting
 tokens_text = [word_tokenize(str(word)) for word in train_data.lower]
 #Unique word counter
 tokens_counter = [item for sublist in tokens_text for item in sublist]
 print("Number of tokens: ", len(set(tokens_counter)))
-
-
-# In[23]:
-
 
 tokens_text
 tokens_counter 
@@ -254,34 +172,15 @@ tokens_counter
 
 # ## TfidfVectorizer
 
-# In[24]:
-
-
 tf=TfidfVectorizer(max_features=5000)
 x=tf.fit_transform(train_data['lower'])
 y=train_data['type']
 
-
-# In[25]:
-
-
 x.shape
-
-
-# In[26]:
-
 
 y.shape
 
-
-# In[27]:
-
-
 X_train,X_test,y_train,y_test=train_test_split(x,y,test_size=0.2,shuffle=True,random_state=42)
-
-
-# In[28]:
-
 
 models = {
     'Naive Bayes': MultinomialNB(),
@@ -296,10 +195,6 @@ for name, model in models.items():
     print("Accuracy:", accuracy_score(y_test, y_pred))
     print("\nClassification Report:\n", classification_report(y_test, y_pred))
     print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred))
-
-
-# In[33]:
-
 
 disp = ConfusionMatrixDisplay.from_predictions(y_test, y_pred, cmap='coolwarm', normalize='true')
 disp.figure_.set_facecolor('white') 
